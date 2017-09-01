dac3016a47054ffca6d5b38e8677178f3006db9a
Tiago Albineli Motta <timotta@gmail.com>
2017-09-01 10:46:38 -0300

    more tests for compute_mrr

--- tests/mrr_test.py
+++ tests/mrr_test.py
@@ -22,3 +22,23 @@ def test_compute_mrr():
 
   assert mrr == 1
   
+def test_compute_mrr_when_dont_predicted_for_user():
+  row = np.array([0,   0,   1])
+  col = np.array([0,   1,   2])
+  val = np.array([1.0, 2.0, 2.0])
+  data = csr_matrix((val, (row, col)), shape=(2, 3))
+  
+  U = np.array([
+    [1.0, 2.0],
+    [2.0, 1.0]
+  ])
+  V = np.array([
+    [2.0, 1.0],
+    [1.0, 2.0],
+    [0.0, 0.0],
+  ])
+  
+  mrr = compute_mrr(data, U, V)
+
+  assert mrr == (1.0 + 1.0/3.0)/2.0
+  
